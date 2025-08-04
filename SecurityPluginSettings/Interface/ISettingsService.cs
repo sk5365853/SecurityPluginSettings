1b998eb9f263f67c550cffde6efce9864a251382
Alessandra Vieira <Alessandra.De_Morais_Vieira@sita.aero>
2025-08-04 19:28:36 +0200

    code cleaning

--- SecurityPluginSettings/Interface/ISettingsService.cs
+++ SecurityPluginSettings/Interface/ISettingsService.cs
@@ -1,8 +1,5 @@
 ﻿using SecurityPluginSettings.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SecurityPluginSettings.Interface
